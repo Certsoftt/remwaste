e8c5f94d61eea741bfd25cdee64a9313f76fef06
Certsoftt <ukpeubong8573@gmail.com>
2025-06-07 01:11:04 +0100

    Fix: base path to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-});
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['mixed-decls'],
+      },
+    },
+  },
+})
