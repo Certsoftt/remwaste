6601a52ffcaac728613c4b96f6b337d5f18f6bf3
Certsoftt <ukpeubong8573@gmail.com>
2025-06-07 06:21:11 +0100

    Fix:CI server Linting Errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['mixed-decls'],
+        silenceDeprecations: ["mixed-decls"],
       },
     },
   },
-})
+});
